6f15e221794b4136d604cdbeac2a97aa648cfd38
Philip Ly <philiply@berkeley.edu>
2015-01-02 14:41:59 -0800

    relocated personal intro to contact me page
    moved personal description to contact me page above social media links.
    hoping to put more portfolio stuff in home page

--- static/js/controllers/mainController.js
+++ static/js/controllers/mainController.js
@@ -1,10 +1,13 @@
 mainApp.controller('homeController', function($scope) {
+    var slides = {
+        1:'honeycombMacro4.jpg',
+        2:'droppedPokeballs.jpg'
+    }
+});
+
+mainApp.controller('contactController', function($scope) {
     $scope.updatePicture = function(target) {
         $('#mainSelfPhoto').removeClass('selfPhoto1').removeClass('selfPhoto2').removeClass('selfPhoto3')
         .addClass('selfPhoto'+$(target).data('photo'));
     };
-});
-
-mainApp.controller('contactController', function($scope) {
-    
 });
